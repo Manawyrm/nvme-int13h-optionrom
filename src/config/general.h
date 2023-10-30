9101549dc78220890d09fc3e703cdef3e0231982
Manawyrm <git@tbspace.de>
2023-10-30 22:00:53 +0100

    Slimdown to 47kiB

--- src/config/general.h
+++ src/config/general.h
@@ -34,70 +34,70 @@ FILE_LICENCE ( GPL2_OR_LATER_OR_UBDL );
  *
  */
 
-#define	NET_PROTO_IPV4		/* IPv4 protocol */
-//#define NET_PROTO_IPV6	/* IPv6 protocol */
+#undef	NET_PROTO_IPV4		/* IPv4 protocol */
+#undef NET_PROTO_IPV6	/* IPv6 protocol */
 #undef	NET_PROTO_FCOE		/* Fibre Channel over Ethernet protocol */
-#define	NET_PROTO_STP		/* Spanning Tree protocol */
-#define	NET_PROTO_LACP		/* Link Aggregation control protocol */
-#define	NET_PROTO_EAPOL		/* EAP over LAN protocol */
-//#define NET_PROTO_LLDP	/* Link Layer Discovery protocol */
+#undef	NET_PROTO_STP		/* Spanning Tree protocol */
+#undef	NET_PROTO_LACP		/* Link Aggregation control protocol */
+#undef	NET_PROTO_EAPOL		/* EAP over LAN protocol */
+#undef NET_PROTO_LLDP	/* Link Layer Discovery protocol */
 
 /*
  * PXE support
  *
  */
-//#undef	PXE_STACK		/* PXE stack in iPXE - you want this! */
-//#undef	PXE_MENU		/* PXE menu booting */
+#undef	PXE_STACK		/* PXE stack in iPXE - you want this! */
+#undef	PXE_MENU		/* PXE menu booting */
 
 /*
  * Download protocols
  *
  */
 
-#define	DOWNLOAD_PROTO_TFTP	/* Trivial File Transfer Protocol */
-#define	DOWNLOAD_PROTO_HTTP	/* Hypertext Transfer Protocol */
+#undef	DOWNLOAD_PROTO_TFTP	/* Trivial File Transfer Protocol */
+#undef	DOWNLOAD_PROTO_HTTP	/* Hypertext Transfer Protocol */
 #undef	DOWNLOAD_PROTO_HTTPS	/* Secure Hypertext Transfer Protocol */
 #undef	DOWNLOAD_PROTO_FTP	/* File Transfer Protocol */
 #undef	DOWNLOAD_PROTO_SLAM	/* Scalable Local Area Multicast */
 #undef	DOWNLOAD_PROTO_NFS	/* Network File System Protocol */
-//#undef DOWNLOAD_PROTO_FILE	/* Local filesystem access */
+#undef DOWNLOAD_PROTO_FILE	/* Local filesystem access */
 
 /*
  * SAN boot protocols
  *
  */
 
-//#undef	SANBOOT_PROTO_ISCSI	/* iSCSI protocol */
-//#undef	SANBOOT_PROTO_AOE	/* AoE protocol */
-//#undef	SANBOOT_PROTO_IB_SRP	/* Infiniband SCSI RDMA protocol */
-//#undef	SANBOOT_PROTO_FCP	/* Fibre Channel protocol */
-//#undef	SANBOOT_PROTO_HTTP	/* HTTP SAN protocol */
+#undef	SANBOOT_PROTO_ISCSI	/* iSCSI protocol */
+#undef	SANBOOT_PROTO_AOE	/* AoE protocol */
+#undef	SANBOOT_PROTO_IB_SRP	/* Infiniband SCSI RDMA protocol */
+#undef	SANBOOT_PROTO_FCP	/* Fibre Channel protocol */
+#undef	SANBOOT_PROTO_HTTP	/* HTTP SAN protocol */
 #define SANBOOT_PROTO_NVME
 
 /*
  * HTTP extensions
  *
  */
-#define HTTP_AUTH_BASIC		/* Basic authentication */
-#define HTTP_AUTH_DIGEST	/* Digest authentication */
-//#define HTTP_AUTH_NTLM	/* NTLM authentication */
-//#define HTTP_ENC_PEERDIST	/* PeerDist content encoding */
-//#define HTTP_HACK_GCE		/* Google Compute Engine hacks */
+#undef HTTP_AUTH_BASIC		/* Basic authentication */
+#undef HTTP_AUTH_DIGEST	/* Digest authentication */
+#undef HTTP_AUTH_NTLM	/* NTLM authentication */
+#undef HTTP_ENC_PEERDIST	/* PeerDist content encoding */
+#undef HTTP_HACK_GCE		/* Google Compute Engine hacks */
 
 /*
  * 802.11 cryptosystems and handshaking protocols
  *
  */
-#define	CRYPTO_80211_WEP	/* WEP encryption (deprecated and insecure!) */
-#define	CRYPTO_80211_WPA	/* WPA Personal, authenticating with passphrase */
-#define	CRYPTO_80211_WPA2	/* Add support for stronger WPA cryptography */
+#undef	CRYPTO_80211_WEP	/* WEP encryption (deprecated and insecure!) */
+#undef	CRYPTO_80211_WPA	/* WPA Personal, authenticating with passphrase */
+#undef	CRYPTO_80211_WPA2	/* Add support for stronger WPA cryptography */
 
 /*
  * Name resolution modules
  *
  */
 
-#define	DNS_RESOLVER		/* DNS resolver */
+#undef	DNS_RESOLVER		/* DNS resolver */
 
 /*
  * Image types
@@ -106,62 +106,68 @@ FILE_LICENCE ( GPL2_OR_LATER_OR_UBDL );
  * you want to use.
  *
  */
-//#define	IMAGE_NBI		/* NBI image support */
-//#define	IMAGE_ELF		/* ELF image support */
-//#define	IMAGE_MULTIBOOT		/* MultiBoot image support */
-//#define	IMAGE_PXE		/* PXE image support */
-//#define	IMAGE_SCRIPT		/* iPXE script image support */
-//#define	IMAGE_BZIMAGE		/* Linux bzImage image support */
-//#define	IMAGE_COMBOOT		/* SYSLINUX COMBOOT image support */
-//#define	IMAGE_EFI		/* EFI image support */
-//#define	IMAGE_SDI		/* SDI image support */
-//#define	IMAGE_PNM		/* PNM image support */
-#define	IMAGE_PNG		/* PNG image support */
-#define	IMAGE_DER		/* DER image support */
-#define	IMAGE_PEM		/* PEM image support */
-//#define	IMAGE_ZLIB		/* ZLIB image support */
-//#define	IMAGE_GZIP		/* GZIP image support */
+#undef	IMAGE_NBI		/* NBI image support */
+#undef	IMAGE_ELF		/* ELF image support */
+#undef	IMAGE_MULTIBOOT		/* MultiBoot image support */
+#define	IMAGE_PXE		/* PXE image support */
+#undef	IMAGE_SCRIPT		/* iPXE script image support */
+#undef	IMAGE_BZIMAGE		/* Linux bzImage image support */
+#undef	IMAGE_COMBOOT		/* SYSLINUX COMBOOT image support */
+#undef	IMAGE_EFI		/* EFI image support */
+#undef	IMAGE_SDI		/* SDI image support */
+#undef	IMAGE_PNM		/* PNM image support */
+#undef	IMAGE_PNG		/* PNG image support */
+#undef	IMAGE_DER		/* DER image support */
+#undef	IMAGE_PEM		/* PEM image support */
+#undef	IMAGE_ZLIB		/* ZLIB image support */
+#undef	IMAGE_GZIP		/* GZIP image support */
 
 /*
  * Command-line commands to include
  *
  */
-#define	AUTOBOOT_CMD		/* Automatic booting */
-#define	NVO_CMD			/* Non-volatile option storage commands */
-#define	CONFIG_CMD		/* Option configuration console */
-#define	IFMGMT_CMD		/* Interface management commands */
-#define	IWMGMT_CMD		/* Wireless interface management commands */
-#define IBMGMT_CMD		/* Infiniband management commands */
-#define FCMGMT_CMD		/* Fibre Channel management commands */
-#define	ROUTE_CMD		/* Routing table management commands */
-#define IMAGE_CMD		/* Image management commands */
-#define DHCP_CMD		/* DHCP management commands */
-#define SANBOOT_CMD		/* SAN boot commands */
-#define MENU_CMD		/* Menu commands */
-#define LOGIN_CMD		/* Login command */
-#define SYNC_CMD		/* Sync command */
-#define SHELL_CMD		/* Shell command */
-//#define NSLOOKUP_CMD		/* DNS resolving command */
-//#define TIME_CMD		/* Time commands */
-//#define DIGEST_CMD		/* Image crypto digest commands */
-//#define LOTEST_CMD		/* Loopback testing commands */
-//#define VLAN_CMD		/* VLAN commands */
-//#define PXE_CMD		/* PXE commands */
-//#define REBOOT_CMD		/* Reboot command */
-//#define POWEROFF_CMD		/* Power off command */
-//#define IMAGE_TRUST_CMD	/* Image trust management commands */
-#define PCI_CMD		/* PCI commands */
-//#define PARAM_CMD		/* Request parameter commands */
-//#define NEIGHBOUR_CMD		/* Neighbour management commands */
-//#define PING_CMD		/* Ping command */
-//#define CONSOLE_CMD		/* Console command */
-//#define IPSTAT_CMD		/* IP statistics commands */
-//#define PROFSTAT_CMD		/* Profiling commands */
-//#define NTP_CMD		/* NTP commands */
-//#define CERT_CMD		/* Certificate management commands */
-//#define IMAGE_MEM_CMD		/* Read memory command */
-#define IMAGE_ARCHIVE_CMD	/* Archive image management commands */
-#define SHIM_CMD		/* EFI shim command (or dummy command) */
+#undef	AUTOBOOT_CMD		/* Automatic booting */
+#undef	NVO_CMD			/* Non-volatile option storage commands */
+#undef	CONFIG_CMD		/* Option configuration console */
+#undef	IFMGMT_CMD		/* Interface management commands */
+#undef	IWMGMT_CMD		/* Wireless interface management commands */
+#undef IBMGMT_CMD		/* Infiniband management commands */
+#undef FCMGMT_CMD		/* Fibre Channel management commands */
+#undef	ROUTE_CMD		/* Routing table management commands */
+#undef IMAGE_CMD		/* Image management commands */
+#undef DHCP_CMD		/* DHCP management commands */
+#undef SANBOOT_CMD		/* SAN boot commands */
+#undef MENU_CMD		/* Menu commands */
+#undef LOGIN_CMD		/* Login command */
+#undef SYNC_CMD		/* Sync command */
+#undef SHELL_CMD		/* Shell command */
+#undef NSLOOKUP_CMD		/* DNS resolving command */
+#undef TIME_CMD		/* Time commands */
+#undef DIGEST_CMD		/* Image crypto digest commands */
+#undef LOTEST_CMD		/* Loopback testing commands */
+#undef VLAN_CMD		/* VLAN commands */
+#undef PXE_CMD		/* PXE commands */
+#undef REBOOT_CMD		/* Reboot command */
+#undef POWEROFF_CMD		/* Power off command */
+#undef IMAGE_TRUST_CMD	/* Image trust management commands */
+#undef PCI_CMD		/* PCI commands */
+#undef PARAM_CMD		/* Request parameter commands */
+#undef NEIGHBOUR_CMD		/* Neighbour management commands */
+#undef PING_CMD		/* Ping command */
+#undef CONSOLE_CMD		/* Console command */
+#undef IPSTAT_CMD		/* IP statistics commands */
+#undef PROFSTAT_CMD		/* Profiling commands */
+#undef NTP_CMD		/* NTP commands */
+#undef CERT_CMD		/* Certificate management commands */
+#undef IMAGE_MEM_CMD		/* Read memory command */
+#undef IMAGE_ARCHIVE_CMD	/* Archive image management commands */
+#undef SHIM_CMD		/* EFI shim command (or dummy command) */
+#undef CPUID_CMD
+
+#undef ENTROPY_RTC
+#undef ENTROPY_RDRAND
+#undef REBOOT_PCBIOS
+
 
 /*
  * ROM-specific options
@@ -174,8 +180,8 @@ FILE_LICENCE ( GPL2_OR_LATER_OR_UBDL );
  * Virtual network devices
  *
  */
-#define VNIC_IPOIB		/* Infiniband IPoIB virtual NICs */
-//#define VNIC_XSIGO		/* Infiniband Xsigo virtual NICs */
+#undef VNIC_IPOIB		/* Infiniband IPoIB virtual NICs */
+#undef VNIC_XSIGO		/* Infiniband Xsigo virtual NICs */
 
 /*
  * Error message tables to include
@@ -201,7 +207,7 @@ FILE_LICENCE ( GPL2_OR_LATER_OR_UBDL );
 #undef	GDBUDP			/* Remote GDB debugging over UDP
 				 * (both may be set) */
 //#define EFI_DOWNGRADE_UX	/* Downgrade UEFI user experience */
-#define	TIVOLI_VMM_WORKAROUND	/* Work around the Tivoli VMM's garbling of SSE
+#undef	TIVOLI_VMM_WORKAROUND	/* Work around the Tivoli VMM's garbling of SSE
 				 * registers when iPXE traps to it due to
 				 * privileged instructions */
 
